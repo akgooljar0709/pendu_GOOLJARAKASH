62bd48a762e60ccd38346b8d961bd3daa3d6f4a3
admin123 <akashgooljar07@gmail.com>
2021-04-19 22:51:46 +0400

    [dev]-Jeux_Pendu

---
+++ include/dico.h
@@ -0,0 +1,25 @@
+/*
+Jeu du pendu
+Par M@teo21, pour le Site du Zér0
+www.siteduzero.com
+
+dico.h
+------
+
+Contient les prototypes des fonctions de dico.c
+*/
+
+
+#ifndef DEF_DICO
+#define DEF_DICO
+
+
+int piocherMot(char *motPioche);
+int nombreAleatoire(int nombreMax);
+
+int gagne(int lettreTrouvee[], long tailleMot);
+int rechercheLettre(char lettre, char motSecret[], int lettreTrouvee[]);
+char lireCaractere();
+
+#endif
+
